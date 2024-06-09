60ada739537d685a3a502b42ad530dabdd68d268
ArfaMujahid <arfamujahid12@gmail.com>
2024-06-09 13:24:55 +0500

    complete

---
+++ backend/config.js
@@ -0,0 +1,10 @@
+const config = {
+  Database: {
+    host: "localhost",
+    user: "root",
+    password: "Arfasara1624928",
+    database: "WebProject",
+  },
+  listPerPage: 10,
+};
+module.exports = config;
